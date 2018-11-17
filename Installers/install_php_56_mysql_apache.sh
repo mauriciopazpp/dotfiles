1699a92f1fe4f18f111901ea0eb753027e076a2f
Mauricio Paz <mauriciopazdev@gmail.com>
2018-11-17 00:47:18 -0200

    php xml added

--- Installers/install_php_56_mysql_apache.sh
+++ Installers/install_php_56_mysql_apache.sh
@@ -9,7 +9,7 @@ else
     apt-get update -y
 
     echo "Installing apache php5.6 common, php5.6 cli, libapache2-mod-php5.6, mbstring..."
-    apt-get install apache2 php5.6-common php5.6-cli php5.6 libapache2-mod-php5.6 php5.6-mbstring -y
+    apt-get install apache2 php5.6-common php5.6-cli php5.6 libapache2-mod-php5.6 php5.6-mbstring php-xml -y
     
     echo "Installing mysql to php5.6"
     apt-get install php5.6-mysql -y
